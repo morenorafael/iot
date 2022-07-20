e646c6ce4b02f086e637d3b2fb5c1525dd440e1f
Rafael Moreno <rafael.moreno@zinobe.com>
2022-07-20 18:42:18 -0500

    refactor

--- src/Home.php
+++ src/Home.php
@@ -2,9 +2,11 @@
 
 namespace Morenorafael\Iot;
 
+use Morenorafael\Iot\Contracts\Commands;
+
 class Home
 {
-    protected string $deviceId;
+    protected Commands $control;
 
     public function __construct(protected \Morenorafael\Iot\Contracts\Repository $repository)
     {
@@ -12,35 +14,35 @@ class Home
 
     public function device(string $deviceId): self
     {
-        $this->deviceId = $deviceId;
+        $this->control = $this->repository->generalDevicesControl($deviceId);
 
         return $this;
     }
 
-    public function on()
+    public function on(string $code = 'switch_1')
     {
-        return $this->repository->generalDevicesControl($this->deviceId)->sendCommands([
-            'code' => 'switch_1',
+        return $this->control->sendCommands([
+            'code' => $code,
             'value' => true,
         ]);
     }
 
-    public function off(?int $ttl = null)
+    public function off(string $code = 'switch_1', ?int $ttl = null)
     {
         if (!is_null($ttl)) {
             return $this->countdown($ttl);
         }
 
-        return $this->repository->generalDevicesControl($this->deviceId)->sendCommands([
-            'code' => 'switch_1',
+        return $this->control->sendCommands([
+            'code' => $code,
             'value' => false,
         ]);
     }
 
-    public function countdown(int $ttl)
+    public function countdown(int $ttl, string $code = 'countdown_1')
     {
-        return $this->repository->generalDevicesControl($this->deviceId)->sendCommands([
-            'code' => 'countdown_1',
+        return $this->control->sendCommands([
+            'code' => $code,
             'value' => $ttl,
         ]);
     }
